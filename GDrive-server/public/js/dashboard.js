8d780171c7c996828390e125b15e0d22e78ee912
huangyuan3h <huangyuan3h@gmail.com>
2017-01-12 00:53:14 +0800

    add folder

--- GDrive-server/public/js/dashboard.js
+++ GDrive-server/public/js/dashboard.js
@@ -8,6 +8,8 @@ var dashboard = {
       method: "get",
       success: function (res) {
 
+        dashboard.loadingRootFiles(res.files);
+
         console.log(res);
 
       },
@@ -15,9 +17,42 @@ var dashboard = {
         console.log(res);
       }
     })
+  },
+  loadingRootFiles:function(files){
+      for (var key in files){
+        var html=dashboard.singleFile(files[key]);
+        $(".folder-container").append(html);
+      }
+  },
+  singleFile:function(file){
+    var fileHtml = '<div class="panel panel-default folder-single col-md-3">' +
+      '<div class="panel-body text-center">' +
+      '<div class="folder-img-container">' ;
+    if(file.mimeType==="application/vnd.google-apps.folder")
+    {
+      fileHtml +='<img src="/img/folder.png" class="folder-img">';
+    }else if(file.mimeType==="application/vnd.google-apps.document"){
+      fileHtml +='<img src="/img/doc.png" class="folder-img">';
+    }else {
+      fileHtml +='<img src="/img/normalfile.png" class="folder-img">';
+    }
+
+    fileHtml += '</div>' +
+      '<div class="col-md-12">'+file.name+'</div>' +
+      '<div class="col-md-12"><span class="label label-default">important</span></div>' +
+      '<div class="col-md-12">' +
+      '<a class="btn btn-primary">view</a>' +
+      '<a class="btn btn-primary">download</a>' +
+      '</div>' +
+      '</div></div>';
+
+      return fileHtml;
   }
 };
 
 $(function () {
   dashboard.init();
 });
+
+
+
