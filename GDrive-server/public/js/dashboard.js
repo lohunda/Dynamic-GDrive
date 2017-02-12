12d95a2e9681214d7456917b891b16ad2662bb51
huangyuan3h <huangyuan3h@gmail.com>
2017-02-12 11:48:22 +0800

    add dashboard code

--- GDrive-server/public/js/dashboard.js
+++ GDrive-server/public/js/dashboard.js
@@ -1,101 +1,126 @@
 var dashboard = {
-  init: function init() {
-    dashboard.getRootFiles();
+    init: function init() {
+      dashboard.getRootFiles();
 
-    $('#upload').click(function () {
-      debugger
-      var formData = new FormData();
-      var files = $('#input-file')[0].files;
-      if (files.length == 0) {
-        alert('not files are selected.')
-        return;
-      }
-      for (var key in files) {
-        formData.append('file' + key, files[key]);
-      }
-      $.ajax({
-        url: '/GDrive/upload',
-        data: formData,
-        contentType: false,
-        processData: false,
-        type: 'post',
-        success: function (res) {
-          alert('upload success');
-          location.reload();
-        },
-        error: function (err) {
-          alert('upload error');
-          console.error(err);
+      dashboard.initToDo();
+
+
+      $('#upload').click(function () {
+        var formData = new FormData();
+        var files = $('#input-file')[0].files;
+        if (files.length == 0) {
+          alert('not files are selected.')
+          return;
         }
-      });
-    })
+        for (var key in files) {
+          formData.append('file' + key, files[key]);
+        }
+        $.ajax({
+          url: '/GDrive/upload',
+          data: formData,
+          contentType: false,
+          processData: false,
+          type: 'post',
+          success: function (res) {
+            alert('upload success');
+            location.reload();
+          },
+          error: function (err) {
+            alert('upload error');
+            console.error(err);
+          }
+        });
+      })
 
 
-  },
-  getRootFiles: function () {
-    $.ajax({
-      url: "/GDrive/getRootFiles",
-      method: "get",
-      success: function (res) {
+    },
+    getRootFiles: function () {
+      $.ajax({
+        url: "/GDrive/getRootFiles",
+        method: "get",
+        success: function (res) {
 
-        dashboard.loadingRootFiles(res.files);
+          dashboard.loadingRootFiles(res.files);
 
-        // $('.delete-file').on('click', function () {
-        //   var fileId =  $(this).attr('file-id');
-        //
-        //   $.ajax({
-        //     url: '/GDrive/delete',
-        //     data: {id: fileId},
-        //     type: 'post',
-        //     success: function (res) {
-        //       alert('delete success');
-        //     },
-        //     error: function (err) {
-        //       alert('delete error');
-        //       console.error(err);
-        //     }
-        //   });
-        // });
+          // $('.delete-file').on('click', function () {
+          //   var fileId =  $(this).attr('file-id');
+          //
+          //   $.ajax({
+          //     url: '/GDrive/delete',
+          //     data: {id: fileId},
+          //     type: 'post',
+          //     success: function (res) {
+          //       alert('delete success');
+          //     },
+          //     error: function (err) {
+          //       alert('delete error');
+          //       console.error(err);
+          //     }
+          //   });
+          // });
 
-        console.log(res);
+          console.log(res);
 
-      },
-      error: function (res) {
-        console.log(res);
+        },
+        error: function (res) {
+          console.log(res);
+        }
+      })
+    },
+    loadingRootFiles: function (files) {
+      for (var key in files) {
+        var html = dashboard.singleFile(files[key]);
+        $(".folder-container").append(html);
       }
-    })
-  },
-  loadingRootFiles: function (files) {
-    for (var key in files) {
-      var html = dashboard.singleFile(files[key]);
-      $(".folder-container").append(html);
-    }
-  },
-  singleFile: function (file) {
-    var fileHtml = '<div class="panel panel-default folder-single col-md-3">' +
-      '<div class="panel-body text-center">' +
-      '<div class="folder-img-container">';
-    if (file.mimeType === "application/vnd.google-apps.folder") {
-      fileHtml += '<img src="/img/folder.png" class="folder-img">';
-    } else if (file.mimeType === "application/vnd.google-apps.document") {
-      fileHtml += '<img src="/img/doc.png" class="folder-img">';
-    } else {
-      fileHtml += '<img src="/img/normalfile.png" class="folder-img">';
-    }
+    },
+    singleFile: function (file) {
+      var fileHtml = '<div class="panel panel-default folder-single col-md-3">' +
+        '<div class="panel-body text-center">' +
+        '<div class="folder-img-container">';
+      if (file.mimeType === "application/vnd.google-apps.folder") {
+        fileHtml += '<img src="/img/folder.png" class="folder-img">';
+      } else if (file.mimeType === "application/vnd.google-apps.document") {
+        fileHtml += '<img src="/img/doc.png" class="folder-img">';
+      } else {
+        fileHtml += '<img src="/img/normalfile.png" class="folder-img">';
+      }
 
-    fileHtml += '</div>' +
-      '<div class="col-md-12">' + file.name + '</div>' +
-      '<div class="col-md-12"><span class="label label-default">important</span></div>' +
-      '<div class="col-md-12">' +
-      '<a class="btn btn-primary">view</a>' +
-      '<a class="btn btn-primary">download</a>' +
-      '<a class="btn btn-primary delete-file" file-id="'+ file.id +'">delete</a>' +
-      '</div>' +
-      '</div></div>';
+      fileHtml += '</div>' +
+        '<div class="col-md-12">' + file.name + '</div>' +
+        '<div class="col-md-12"><span class="label label-default">important</span></div>' +
+        '<div class="col-md-12">' +
+        '<a class="btn btn-primary">view</a>' +
+        '<a class="btn btn-primary">download</a>' +
+        '<a class="btn btn-primary delete-file" file-id="' + file.id + '">delete</a>' +
+        '</div>' +
+        '</div></div>';
 
-    return fileHtml;
-  }
-};
+      return fileHtml;
+    },
+    initToDo: function () {
+      var mockData = {
+        'todo1': {
+          name: 'edit jira card 013',
+          selected: false,
+        },
+        'todo2': {
+          name: 'edit jira card 014',
+          selected: false,
+        },
+        'todo3': {
+          name: 'edit jira card 015',
+          selected: false,
+        }
+      };
+
+
+      for (var key in mockData) {
+        $(".js-hook-todo-content").append('<div class="col-md-12 todo-item">' + mockData[key].name + '</div>');
+      }
+
+    }
+}
+
 
 $(function () {
   dashboard.init();
